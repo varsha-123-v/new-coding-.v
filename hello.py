df9ce95074683601c1ae358bc25ae2d17262c64b
varsha-123-v <131846825+varsha-123-v@users.noreply.github.com>
2025-06-10 11:33:18 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
+def prime(n):
+  if n<=1:
+    return False
+  for i in range(2,int(n**0.5)+1):
+    if n%i==0:
+      return False
+  return True
+
+for j in range(2,50):
+  if prime(j):
+    print(j,end=" ")
 
